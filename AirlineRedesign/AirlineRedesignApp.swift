32ede2281f326ae1828adf684baee323fbbe873e
loganpetree <logan.c.petree@gmail.com>
2024-07-08 13:43:40 -0500

    Initial Commit

---
+++ AirlineRedesign/AirlineRedesignApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirlineRedesignApp.swift
+//  AirlineRedesign
+//
+//  Created by Logan Petree on 7/8/24.
+//
+
+import SwiftUI
+
+@main
+struct AirlineRedesignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
